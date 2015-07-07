4d09cfaa88f9d381464057276c236af79f05277c
trogdan <mr.trogdan@gmail.com>
2015-07-06 21:41:41 -0400

    refactor menus, add searchView to mainfragment

--- app/src/main/java/com/trogdan/nanospotify/MainActivityFragment.java
+++ app/src/main/java/com/trogdan/nanospotify/MainActivityFragment.java
@@ -3,6 +3,8 @@ package com.trogdan.nanospotify;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,9 +17,22 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.mainfragment, menu);
     }
 }
